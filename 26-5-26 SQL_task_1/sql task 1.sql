f56a30ab4a40b3935d4325aa624c520441ad5568
shanmuga1003 <shanmugap089@gmail.com>
2026-06-01 20:55:13 +0530

    Delete 26-5-26 SQL_task_1 directory

--- 26-5-26 SQL_task_1/sql task 1.sql
+++
@@ -1,7 +0,0 @@
-Create database schoolDB;
-use schoolDB;
-create table students(id int primary key, name varchar(20) not null, age int, grade char(1));
-insert into students(id,name,age,grade) values (1,'shanmuga',23,'A'),(2,'venkat',22,'A'),(3,'sadham',23,'S'),(4,'sathya',23,'S'),(5,'sherif',23,'B');
-select * from students;
-create database schoolDB2;
-drop database schoolDB2;
